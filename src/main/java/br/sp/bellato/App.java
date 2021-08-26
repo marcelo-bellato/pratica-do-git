6002822de622acad47077fad6922722b7eed5e2c
marcelo-bellato <bellato.consultor@gmail.com>
2021-08-26 15:40:13 -0300

    saudação portugues

--- src/main/java/br/sp/bellato/App.java
+++ src/main/java/br/sp/bellato/App.java
@@ -6,6 +6,7 @@ package br.sp.bellato;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Hola mundo");
+        System.out.println("Ola MUndo!");
+
     }
 }
