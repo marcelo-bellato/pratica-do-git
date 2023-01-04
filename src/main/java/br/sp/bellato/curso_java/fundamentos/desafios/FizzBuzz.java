013867dcce91857047752045ee38e930d5b2478c
marcelo-bellato <bellato.consultor@gmail.com>
2023-01-03 22:32:18 -0300

    desafio FizzBuzz

---
+++ src/main/java/br/sp/bellato/curso_java/fundamentos/desafios/FizzBuzz.java
@@ -0,0 +1,30 @@
+package br.sp.bellato.curso_java.fundamentos.desafios;
+
+
+public class FizzBuzz
+{
+  public static void main(String[] args)
+  {
+    for (int i = 1; i < 30; i++)
+    {
+      if (i % 3 == 0 && i % 5 == 0)
+      {
+        System.out.println("FizzBuzz");
+      }
+      else if (i % 5 == 0)
+      {
+        System.out.println("Buzz");
+      }
+      else if (i % 3 == 0)
+      {
+        System.out.println("Fizz");
+      }
+      else
+      {
+        System.out.println(i);
+      }
+    }
+  }
+
+
+}
